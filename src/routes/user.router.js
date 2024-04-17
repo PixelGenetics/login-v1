da5274df92815b9ab83e9d5be0b445e4ec9eb615
Kevin Antonio Borge Gutierrez <108099077+PixelGenetics@users.noreply.github.com>
2024-04-16 21:08:36 -0600

    First commit

---
+++ src/routes/user.router.js
@@ -0,0 +1,23 @@
+const { getAll, create, getOne, remove, update, veryCode, login} = require('../controllers/user.controller');
+const express = require('express');
+const { verifyJwt } = require('../utils/verifyJWT');
+
+const routerUser = express.Router();
+
+routerUser.route('/')
+    .get(verifyJwt, getAll)
+    .post(create);
+
+routerUser.route('/login')
+    .post(login)
+
+routerUser.route('/verify/:code')
+    .get(veryCode);
+
+
+routerUser.route('/:id')
+    .get(getOne)
+    .delete(remove)
+    .put(update);
+
+module.exports = routerUser;
